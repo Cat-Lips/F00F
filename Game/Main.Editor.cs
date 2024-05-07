1b867aea58a50b14fae63574918b6815e5c5c5b0
Cat-Lips <57929251+Cat-Lips@users.noreply.github.com>
2024-05-07 18:28:57 +1000

    Initial commit

---
+++ Game/Main.Editor.cs
@@ -0,0 +1,11 @@
+﻿//#if TOOLS
+//namespace Game;
+
+//public partial class Main
+//{
+//    protected sealed override void OnEditorSave()
+//    {
+
+//    }
+//}
+//#endif
---
+++ Tests/Main.cs
@@ -0,0 +1,9 @@
+//using F00F;
+//using Godot;
+
+//namespace Tests;
+
+//[Tool]
+//public partial class Main : Test3D
+//{
+//}
